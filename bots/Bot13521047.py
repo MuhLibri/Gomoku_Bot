3bcd36a5fa1497d50a093570884c33e510319307
Muhammad Equilibrie Fajria <104043362+MuhLibri@users.noreply.github.com>
2023-08-03 22:23:40 +0700

    can move horizontal

--- bots/Bot13521047.py
+++ bots/Bot13521047.py
@@ -63,6 +63,224 @@ class Bot13521047(object):
 
             TODO: Tentukan x,y secara greedy. Kembalian adalah sebuah string "x,y"
         """
-        x = random.randint(1, board.height)
-        y = random.randint(1, board.width)
+        myPositions, enemyPositions = self.get_positions(board.states)
+        myPositions.sort()
+        enemyPositions.sort()
+        myPositions = [self.convert_to_coordinate(x) for x in myPositions]
+        enemyPositions = [self.convert_to_coordinate(x) for x in enemyPositions]
+
+        print("1")
+        myHorizontal = self.get_horizontal(myPositions)
+        myHorizontal.sort(key=lambda x: len(x))
+        myVertical = self.get_vertical(myPositions)
+        myVertical.sort(key=lambda x: len(x))
+        myDiagonal1 = self.get_diagonal1(myPositions)
+        myDiagonal1.sort(key=lambda x: len(x))
+        myDiagonal2 = self.get_diagonal2(myPositions)
+        myDiagonal2.sort(key=lambda x: len(x))
+        print("2")
+        # print(myHorizontal)
+        # print(myVertical)
+        # print(myDiagonal1)
+        # print(myDiagonal2)
+        longestSide, longestLine = self.get_longest_line(myHorizontal, myVertical, myDiagonal1, myDiagonal2)
+        action = self.get_next_position(longestSide, longestLine, enemyPositions)
+
+        # while (action == (-1,-1) and len(myHorizontal) != 0 and len(myVertical) != 0 and len(myDiagonal1) != 0 and len(myDiagonal2) != 0):
+        #     if (longestSide == "horizontal"):
+        #         myHorizontal.
+        print("3")
+        if (action == (-1, -1)):
+            x = random.randint(1, board.height)
+            y = random.randint(1, board.width)
+        else:
+            x = action[0]
+            y = action[1]
+
+        # x = random.randint(1, board.height)
+        # y = random.randint(1, board.width)
+
         return f"{x},{y}"
+
+    def get_positions(self, state: dict):
+        myPositions = []
+        enemyPositions = []
+
+        for p in state:
+            if (state[p] == self.player):
+                myPositions.append(p)                
+            else:
+                enemyPositions.append(p)
+        
+        return (myPositions, enemyPositions)
+
+    def convert_to_coordinate(self, num: int):
+        return (num // 8, num % 8)
+
+    def get_horizontal(self, positions: list):
+        horizontalList = []
+        if (len(positions) != 0):
+            tempList = [positions[0]]
+            i = 1
+            j = 0
+
+            while (i < len(positions)):
+                if (positions[i][0] == tempList[j][0] and (abs(positions[i][1] - tempList[j][1]) == 1)):
+                    tempList.append(positions[i])
+                    j = j + 1
+                else:
+                    horizontalList.append(tempList.copy())
+                    tempList = [positions[i]]
+                    j = 0
+                i = i + 1
+        
+            horizontalList.append(tempList.copy())
+
+        return horizontalList
+
+    def get_vertical(self, positions: list):
+        verticalList = []
+        tempList = []
+        
+        for y in range (8):
+            prevExist = False
+            for x in range (9):
+                currentPosition = (x,y)
+                if (currentPosition in positions):
+                    tempList.append(currentPosition)
+                    prevExist = True
+                elif (not currentPosition in positions and prevExist):
+                    prevExist = False
+                    verticalList.append(tempList.copy())
+                    tempList.clear()
+
+        return verticalList
+    
+    def get_diagonal1(self, positions: list):
+        diagonal1List = []
+        tempList = []
+
+        for y in range (8):
+            x = 0
+            prevExist = False
+            while (x < 9 and y < 9):
+                currentPosition = (x, y)
+                if (currentPosition in positions):
+                    tempList.append(currentPosition)
+                    prevExist = True
+                elif (not currentPosition in positions and prevExist):
+                    prevExist = False
+                    diagonal1List.append(tempList.copy())
+                    tempList.clear()
+                x += 1
+                y += 1
+
+        for x in range (1,8):
+            y = 0
+            prevExist = False
+            while (x < 9 and y < 9):
+                currentPosition = (x, y)
+                if (currentPosition in positions):
+                    tempList.append(currentPosition)
+                    prevExist = True
+                elif (not currentPosition in positions and prevExist):
+                    prevExist = False
+                    diagonal1List.append(tempList.copy())
+                    tempList.clear()
+                x += 1
+                y += 1
+
+        return diagonal1List
+    
+
+    def get_diagonal2(self, positions: list):
+        diagonal1List = []
+        tempList = []
+
+        for y in range (7, -1, -1):
+            x = 0
+            prevExist = False
+            while (x < 9 and y > -2):
+                currentPosition = (x, y)
+                if (currentPosition in positions):
+                    tempList.append(currentPosition)
+                    prevExist = True
+                elif (not currentPosition in positions and prevExist):
+                    prevExist = False
+                    diagonal1List.append(tempList.copy())
+                    tempList.clear()
+                x += 1
+                y -= 1
+
+        for x in range (1,8):
+            y = 7
+            prevExist = False
+            while (x < 9 and y > -1):
+                currentPosition = (x, y)
+                if (currentPosition in positions):
+                    tempList.append(currentPosition)
+                    prevExist = True
+                elif (not currentPosition in positions and prevExist):
+                    prevExist = False
+                    diagonal1List.append(tempList.copy())
+                    tempList.clear()
+                x += 1
+                y -= 1
+
+        return diagonal1List
+    
+    def get_longest_line(self, myHorizontal, myVertical, myDiagonal1, myDiagonal2):
+        longestSide = "none"
+        longestLine = 0
+        if (len(myHorizontal) != 0):
+            longestSide = "horizontal"
+            longestLine = myHorizontal[0]
+            if (len(myVertical[0]) > len(longestLine)):
+                longestSide = "vertical"
+                longestLine = myVertical[0]
+            if (len(myDiagonal1[0]) > len(longestLine)):
+                longestSide = "diagonal1"
+                longestLine = myDiagonal1[0]
+            if (len(myDiagonal2[0]) > len(longestLine)):
+                longestSide = "diagonal2"
+                longestLine = myDiagonal2[0]
+        return longestSide, longestLine
+
+
+    def get_next_position(self, longestSide, longestLine, enemyPositions):
+        if (longestSide == "horizontal"):
+            leftSide = (longestLine[0][0], longestLine[0][1]-1)
+            if (not leftSide in enemyPositions and self.is_input_valid(leftSide)):
+                return leftSide
+            rightSide = (longestLine[-1][0], longestLine[-1][1]+1)
+            if (not rightSide in enemyPositions and self.is_input_valid(rightSide)):
+                return rightSide
+
+        elif (longestSide == "vertical"):
+            upSide = (longestLine[0][0]+1, longestLine[0][1])
+            if (not upSide in enemyPositions and self.is_input_valid(upSide)):
+                return upSide
+            bottomSide = (longestLine[-1][0]-1, longestLine[-1][1])
+            if (not bottomSide in enemyPositions and self.is_input_valid(bottomSide)):
+                return bottomSide
+
+        elif (longestSide == "diagonal1"):
+            upSide = (longestLine[0][0]+1, longestLine[0][1]-1)
+            if (not upSide in enemyPositions and self.is_input_valid(upSide)):
+                return upSide
+            bottomSide = (longestLine[-1][0]-1, longestLine[-1][1]+1)
+            if (not bottomSide in enemyPositions and self.is_input_valid(bottomSide)):
+                return bottomSide
+
+        elif (longestSide == "diagonal2"):
+            upSide = (longestLine[0][0]+1, longestLine[0][1]+1)
+            if (not upSide in enemyPositions and self.is_input_valid(upSide)):
+                return upSide
+            bottomSide = (longestLine[-1][0]-1, longestLine[-1][1]-1)
+            if (not bottomSide in enemyPositions and self.is_input_valid(bottomSide)):
+                return bottomSide
+
+        return (-1,-1)
+
+    def is_input_valid(self, position):
+        return (0 <= position[0] <= 7) and (0 <= position[1] <= 7)
